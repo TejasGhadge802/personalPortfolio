44b15de3d7ed295d36dc1503d411165122630477
Tejas Ghadge <ghadgetejas802@gmail.com>
2025-04-26 12:42:16 +0530

    Cards Swipe effect Update main.js

--- main.js
+++ main.js
@@ -7,3 +7,31 @@ menuToggle.addEventListener("click", () => {
     navLinks.classList.toggle("show");
 });
 
+
+//.                      Cards Swipe Logic
+const track = document.querySelector('.cardsTrack');
+const cards = document.querySelectorAll('.cards');
+const btnNext = document.getElementById('btnNext');
+const btnPre = document.getElementById('btnPre');
+
+let currentIndex = 0;
+const totalCards = cards.length;
+
+btnNext.addEventListener('click', () => {
+    if (currentIndex < totalCards - 1) {
+        currentIndex++;
+        updateCardPosition();
+    }
+});
+
+btnPre.addEventListener('click', () => {
+    if (currentIndex > 0) {
+        currentIndex--;
+        updateCardPosition();
+    }
+});
+
+function updateCardPosition() {
+    track.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
